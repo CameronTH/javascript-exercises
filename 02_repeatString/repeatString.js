f546e4133bccc4cd327ed234b31cc5424d241dcb
CameronTH <cameronthomson40@gmail.com>
2023-09-26 00:36:47 +0100

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let text = "";
+  if (num < 0) {
+    text = "ERROR";
+  } else {
+    for (num; num > 0; num--) {
+      text = text + string;
+    }
+  }
+  return text;
 };
 
 // Do not edit below this line
